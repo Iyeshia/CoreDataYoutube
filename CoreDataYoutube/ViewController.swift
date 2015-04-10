96a7af7e714fc030a7ec8d09b1640369b9d90e1a
Carper <imc7@clg62-m131.labs.pitt.edu>
2015-04-10 14:35:50 -0400

    Initial Commit

---
+++ CoreDataYoutube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataYoutube
+//
+//  Created by Carper, Iyeshia Milan on 4/10/15.
+//  Copyright (c) 2015 Carper, Iyeshia Milan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
